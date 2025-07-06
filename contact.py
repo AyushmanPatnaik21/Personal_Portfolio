a373cb41cd7e1e267c0c05b71441d91b4c647357
AyushmanPatnaik21 <ayushmanpatnaik2004@gmail.com>
2025-07-06 10:15:42 +0530

    Add files via upload

---
+++ contact.py
@@ -0,0 +1,9 @@
+contact_data={
+     1: {
+        'email': 'ayushmanpatnaik2004@gmail.com',
+        'name': 'Ayushman Patnaik',
+        'Position': 'Python Developer Intern',
+        'github': 'https://github.com/ayushman073',
+        'Phone.No': '+91-9861262630'
+    }
+}
